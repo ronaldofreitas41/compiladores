3671da43f040c0041fa77d2cc8f653f1c3bb7ff4
Bernardo Dias <bernardo.araujo@aluno.ufop.edu.br>
2024-12-01 23:52:49 -0300

    makefile main

---
+++ src/Main.java
@@ -0,0 +1,38 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+public class Main {
+
+    @SuppressWarnings({"CallToPrintStackTrace"})
+    public static void main(String[] args) {
+        if(args.length != 1){
+            help();
+            System.exit(0);
+        }
+
+        try{
+            Analisador lex = new Analisador(new FileReader(args[0]) );
+            Token t = lex.nextToken();
+
+            while(t.tk != TK.EOF){
+                System.out.println(t.toString());
+                t = lex.nextToken();
+            }
+
+            System.out.println(t.toString());
+
+        }catch(FileNotFoundException e){
+            System.out.println("O arquivo " + args[0] + " não foi encontrado.");
+        }catch(Exception e){
+            System.out.println("Erro ao processar " + args[0] );
+            e.printStackTrace();
+        }
+    }
+
+    public static void help(){
+        System.out.println(" Analisador léxico para a linguagem LANG.");
+        System.out.println(" Versão: 0.1");
+        System.out.println(" Executar: make");
+        System.out.println(" Apagar arquivos gerados: make clean");
+    }
+}
---
+++ src/Token.java
@@ -0,0 +1,24 @@
+public class Token {
+
+    public Object value;
+    public TK tk;
+    public int line;
+    public int column;
+
+    public Token(int line, int column, TK t){
+        this.line = line;
+        this.column = column;
+        value = null;
+        tk = t;
+    }
+
+    public Token(int line, int column, TK t, Object v){
+        this(line,column,t);
+        value = v;
+    }
+
+    public String toString(){
+        return "(" + line + "," + column+ ") TK: " + tk +  (value == null ? "" : "  val: " + value.toString());
+    }
+
+}
